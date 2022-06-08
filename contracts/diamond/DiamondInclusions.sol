9a239c19b4426d64cf16875d99edab1796416378
NFTicks <nfticks@gmail.com>
2022-06-08 18:29:58 +0300

    inclusion call modifier

--- contracts/diamond/DiamondInclusions.sol
+++ contracts/diamond/DiamondInclusions.sol
@@ -4,6 +4,33 @@ pragma solidity ^0.8.0;
 import { LibDiamond } from "./libraries/LibDiamond.sol";
 
 contract DiamondInclusions {
+    modifier inclusion() {
+        LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
+        address facet = ds.selectorInfo[msg.sig].facetAddress;
+
+        require(facet != address(0), "DiamondInclusions: Function does not exist");
+        
+        if (facet == address(this)) {
+            _;
+        } else {
+            assembly {
+                calldatacopy(0, 0, calldatasize())
+
+                let result := delegatecall(gas(), facet, 0, calldatasize(), 0, 0)
+
+                returndatacopy(0, 0, returndatasize())
+
+                switch result
+                case 0 {
+                    revert(0, returndatasize())
+                }
+                default {
+                    return(0, returndatasize())
+                }
+            }
+        }
+    }
+
     /**
      * @notice Attempts to upgrade an inclusion function call.
      * @dev Upgrades the call if the inclusion function selector is part of an external facet.
